8e3bfc2eb0e4ca7338816910ec1b0f265801b6d1
Sameer ansari <sameershams.adi@gmail.com>
2023-04-02 02:05:28 +0530

    resp Api salles admin page completed

--- addProduct.js
+++ addProduct.js
@@ -2,12 +2,13 @@ const form = document.getElementById('addItems')
 form.addEventListener('submit',(e)=>{
     e.preventDefault();
     const sellingPrice = document.getElementById('price').value;
+    
     const productName = document.getElementById('productName').value;
     const obj = {
         price:sellingPrice,
         name:productName
     }
-    axios.post('https://crudcrud.com/api/1b3bcaae57554e859c69ef7fed170a65/AppointmentData',obj)
+    axios.post('https://crudcrud.com/api/6219baae77fd417a82d278e592aefafc/AppointmentData',obj)
     .then((responce) => {
         showProductOnScreen(responce.data)
     }).catch((err) => {
@@ -17,36 +18,53 @@ form.addEventListener('submit',(e)=>{
 
 // onload 
 window.addEventListener('DOMContentLoaded',()=>{
-    axios.get('https://crudcrud.com/api/1b3bcaae57554e859c69ef7fed170a65/AppointmentData')
+    axios.get('https://crudcrud.com/api/6219baae77fd417a82d278e592aefafc/AppointmentData')
     .then((responce) => {
         const objKey = Object.keys(responce.data);
     
         for(let i=0; i<objKey.length; i++){
             
             showProductOnScreen(responce.data[i]);
+            
         }
-    }).catch((err) => {
+        }).catch((err) => {
         console.log(err)
     });
 })
 // function of showing product on screen
 
 function showProductOnScreen(product){
+    
     const parentList = document.getElementById('item-list');
     const children = `<li style="color:black;" id='${product._id}'>${product.price}-${product.name}
-                            <button onClick=deleteProduct('${product._id}')>del product</button></li>`;
+                            <button onClick="deleteProduct('${product._id}'); delPrice('${product.price}')">del product</button></li>`;
     parentList.innerHTML += children;
+    addPrice(Number(product.price))
+}
+// add prices
+let sum = 0;
+let div = document.getElementById('addPrice');
+function addPrice(num){
+    sum += num;
+    div.innerHTML = `Net worth is ${sum}Rs`
+}
+
+// delete Price
+function delPrice(pr){
+     sum = sum - pr;
+    div.innerHTML = `Net worth is ${sum}Rs`
 }
 // delete product
 function deleteProduct(productId){
     
-    axios.delete(`https://crudcrud.com/api/1b3bcaae57554e859c69ef7fed170a65/AppointmentData/${productId}`)
+    axios.delete(`https://crudcrud.com/api/6219baae77fd417a82d278e592aefafc/AppointmentData/${productId}`)
     .then(() => {
         removeProductFromScreen(productId);
     }).catch((err) => {
         console.log(err)
     });
  }
+ // remove product from screen
  function removeProductFromScreen(productId){
     const parentNode = document.getElementById('item-list');
     const delNode = document.getElementById(productId);
@@ -55,3 +73,5 @@ function deleteProduct(productId){
     }
 
  }
+
+ 
